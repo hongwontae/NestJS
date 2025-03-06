3711a6acc7fc29bd9151016a52ba402c427170e5
dnjsxoghd <feelchok1234@gmail.com>
2025-03-06 18:15:05 +0900

    Real Testing

--- 1-NestJS-Pratice/code/4-NestJS-TypeORM/src/users/auth.service.spec.ts
+++ 1-NestJS-Pratice/code/4-NestJS-TypeORM/src/users/auth.service.spec.ts
@@ -3,25 +3,42 @@ import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
 
-it('can create an instance of auth service', async () => {
-  // Create a fake copy of the users service
-  const fakeUsersService : Partial<UsersService> = {
-    findUser: () => Promise.resolve([]),
-    createUser: (email: string, password: string) =>
-      Promise.resolve({ id: 1, email, password } as User),
-  };
+let service : AuthService;
 
-  const module = await Test.createTestingModule({
-    providers: [
-      AuthService,
-      {
-        provide: UsersService,
-        useValue: fakeUsersService,
-      },
-    ],
-  }).compile();
+describe('AuthService', () => {
+    beforeEach(async () => {
+        // Create a fake copy of the users service
+      const fakeUsersService : Partial<UsersService> = {
+        findUser: () => Promise.resolve([]),
+        createUser: (email: string, password: string) =>
+          Promise.resolve({ id: 1, email, password } as User),
+      };
+    
+      const module = await Test.createTestingModule({
+        providers: [
+          AuthService,
+          {
+            provide: UsersService,
+            useValue: fakeUsersService,
+          },
+        ],
+      }).compile();
+    
+       service = module.get(AuthService);
+    })
+    
+    it('can create an instance of auth service', async () => {
+      expect(service).toBeDefined();
+    });
 
-  const service = module.get(AuthService);
+    it('create user', async ()=>{
+        const user = await service.signup('dnjsxoghd@naver.com', 'asdf');
+        expect(user.password).not.toEqual('asdf');
+        const [salt, hash] = user.password.split('.');
+        expect(salt).toBeDefined();
+        expect(hash).toBeDefined();
+    })
+    
+})
 
-  expect(service).toBeDefined();
-});
+
