732d68e557cdd1ebcbb98d97d66f84741fc84ee5
dnjsxoghd <feelchok1234@gmail.com>
2025-03-11 12:24:32 +0900

    nest testing

--- 1-NestJS-Pratice/code/4-NestJS-TypeORM/test/auth.e2e-spec.ts
+++ 1-NestJS-Pratice/code/4-NestJS-TypeORM/test/auth.e2e-spec.ts
@@ -17,19 +17,41 @@ describe('Authentication System', () => {
   });
 
   it('handles a signup request', () => {
-    const email = 'asdlkjq1234@akl.com'
+    const email = 'asdlkjq1234@akl.com';
     // 항상 http 서버에 요청을 수행하도록 합니다.
-    return request(app.getHttpServer())
-    // 메서드 호출
-      .post('/auth/signup')
-      .send({email, password : '@lskdfjl'})
-      // 반환되는 응답을 예상합니다.
-      // 200 성공 코드 + return 값
-      .expect(201)
-      .then((res)=>{
-        const {id, email} = res.body;
-        expect(id).toBeDefined();
-        expect(email).toEqual(email)
-      });
+    return (
+      request(app.getHttpServer())
+        // 메서드 호출
+        .post('/auth/signup')
+        .send({ email, password: '@lskdfjl' })
+        // 반환되는 응답을 예상합니다.
+        // 200 성공 코드 + return 값
+        .expect(201)
+        .then((res) => {
+          const { id, email } = res.body;
+          expect(id).toBeDefined();
+          expect(email).toEqual(email);
+        })
+    );
+  });
+
+  it('signup as a new user then get the currently logged in user', async () => {
+    const email = 'dnjsxoghd@naver.com';
+
+    const res = await request(app.getHttpServer())
+    .post('/auth/signup')
+    .send({email, password : 'q2tlxm@123'})
+    .expect(201)
+
+    const cookie = res.get('Set-Cookie');
+
+    const {body} = await request(app.getHttpServer())
+    .get('/auth/whoami')
+    .set('Cookie', cookie as string[])
+    .expect(200)
+
+    expect(body.email).toEqual(email);
+    
+
   });
 });
