d97b5177dc8f87c782ea50ca146b5b0bf801b98a
dnjsxoghd <feelchok1234@gmail.com>
2025-10-26 17:00:34 +0900

    review

--- 1-NestJS-Pratice/code/1-NestJS-Basic/src/app.module.ts
+++ 1-NestJS-Pratice/code/1-NestJS-Basic/src/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
+import { UsssController } from './usss/usss.controller';
+import { UserController } from './user.controller';
+import { UserController } from './user.controller';
+import { UssssModule } from './ussss/ussss.module';
+import { UsssService } from './usss/usss.service';
+import { UssssController } from './ussss/ussss.controller';
+import { UsssController } from './usss/usss.controller';
 
 @Module({
-    controllers : [AppController]
+    controllers : [AppController, UsssController, UssssController, UserController],
+    providers: [UsssService],
+    imports: [UssssModule]
 })
 export class AppModule{
 
